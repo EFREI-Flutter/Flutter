cecf8f08f9844e9ab1f8d8024740bdeb7c9c2ef9
Nikola.Z <nzivkovic@eig.fr>
2025-11-24 11:07:04 +0100

    fix ci

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+// ignore_for_file: prefer_const_constructors
+
 void main() {
   testWidgets('App starts and shows todo home', (tester) async {
     await tester.pumpWidget(
