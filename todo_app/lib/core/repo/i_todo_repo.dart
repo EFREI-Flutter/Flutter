87a40f08dff750d7b237ff2e09772afd1523925d
Mxlaser <66446663+Mxlaser@users.noreply.github.com>
2025-11-24 10:19:28 +0100

    Delete todo_app directory

--- todo_app/lib/core/repo/i_todo_repo.dart
+++
@@ -1,8 +0,0 @@
-import '../models/todo.dart';
-
-abstract class ITodoRepo {
-  Stream<List<Todo>> watchByUid(String uid);      // tri createdAt desc
-  Future<String> add(Todo todo);                   // retourne docId
-  Future<void> toggleDone({required String id, required bool done});
-  Future<void> delete(String id);
-}
